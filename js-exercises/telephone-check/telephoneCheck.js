bf4607a7a144b0b68b7e957fee19ae9e472d362e
Nisham <nisham.mahsin@gmail.com>
2020-04-24 21:30:37 +0530

    correct typo

--- js-exercises/telephone-check/telephoneCheck.js
+++ js-exercises/telephone-check/telephoneCheck.js
@@ -1,7 +1,7 @@
 
-const telephoneCheck = (phoneNumber) => {
+const telephoneCheck = () => {
   if(typeof phoneNumber !== 'string') {
-    throw TypeError(`Expected string. got ${typeof phoneNumbe}`);
+    throw TypeError(`Expected string. got ${typeof phoneNumber}`);
   }
   const re = new RegExp(/^(1\s?)?((\([0-9]{3}\))|[0-9]{3})[\s\-]?[\0-9]{3}[\s\-]?[0-9]{4}$/)
   return re.test(phoneNumber)
