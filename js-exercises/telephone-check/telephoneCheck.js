172d5ba44f470c299e20f9ebee332caa47a010db
Nisham <nisham.mahsin@gmail.com>
2020-04-27 21:15:09 +0530

    change variable name as per review comments

--- js-exercises/telephone-check/telephoneCheck.js
+++ js-exercises/telephone-check/telephoneCheck.js
@@ -3,8 +3,8 @@ const telephoneCheck = () => {
   if(typeof phoneNumber !== 'string') {
     throw TypeError(`Expected string. got ${typeof phoneNumber}`);
   }
-  const re = new RegExp(/^(1\s?)?((\([0-9]{3}\))|[0-9]{3})[\s\-]?[\0-9]{3}[\s\-]?[0-9]{4}$/)
-  return re.test(phoneNumber)
+  const pattern = new RegExp(/^(1\s?)?((\([0-9]{3}\))|[0-9]{3})[\s\-]?[\0-9]{3}[\s\-]?[0-9]{4}$/)
+  return pattern.test(phoneNumber)
  
 }
 
