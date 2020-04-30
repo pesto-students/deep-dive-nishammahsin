b826beb47460e69a6cd3e2b8cb4b73cf253881fd
Nisham <nisham.mahsin@gmail.com>
2020-04-30 12:46:49 +0530

    initial commit
    Create a first setup for Modal react component

---
+++ react-exercises/modal-react/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {Modal} from  './Modal'
+
+function App() {
+  return (
+    <div className="App">
+      <Modal></Modal>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-exercises/modal-react/src/Modal/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Modal = () => {
+    return (
+        <div>
+            This is test modal
+        </div>
+    )
+}
+
+Modal.propTypes = {
+    prop: PropTypes
+}
+
+export {Modal}
