710d90644ab24195a36927e5af63bb08e7071a64
Nisham <nisham.mahsin@gmail.com>
2020-04-30 12:18:08 +0530

    make changes as per review
    1. Proper formating and indentation
    2. change isString check to  typeof

--- js-exercises/abbreviate-string/abbreviateString.js
+++ js-exercises/abbreviate-string/abbreviateString.js
@@ -1,25 +1,26 @@
-import { isString } from "util";
 const getFirstChar = (string) => {
-    if(!isString(string)) {
-        throw TypeError(`Expected string. got ${typeof string}`);
-    } 
- return string.split('')[0];
-}
+  if (typeof string !== "string") {
+    throw TypeError(`Expected string. got ${typeof string}`);
+  }
+  return string.split("")[0];
+};
 
-const  abbreviateString = (string) => {
-    if(!isString(string)) {
-        throw TypeError(`Expected string. got ${typeof string}`);
-    } 
-    if(string.length === 0) {
-        throw TypeError(`Expected a non empty string`);
-    } 
-    const splittedString = string.trim().split(/\s+/);
-    const splittedStringLength = splittedString.length;
-    if(splittedStringLength ===1 ) {
-        return splittedString[0]
-    } else {
-        return `${splittedString[0]} ${getFirstChar(splittedString[splittedStringLength -1]).toUpperCase()}.`;
-    }
-}
+const abbreviateString = (string) => {
+  if (typeof string !== "string") {
+    throw TypeError(`Expected string. got ${typeof string}`);
+  }
+  if (string.length === 0) {
+    throw TypeError(`Expected a non empty string`);
+  }
+  const splittedString = string.trim().split(/\s+/);
+  const splittedStringLength = splittedString.length;
+  if (splittedStringLength === 1) {
+    return splittedString[0];
+  } else {
+    return `${splittedString[0]} ${getFirstChar(
+      splittedString[splittedStringLength - 1]
+    ).toUpperCase()}.`;
+  }
+};
 
 export { abbreviateString };
