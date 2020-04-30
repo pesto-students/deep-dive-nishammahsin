b052f3b0e06821bc51b57ccf70a34373b70f8e61
Nisham <nisham.mahsin@gmail.com>
2020-04-30 14:12:06 +0530

    add modal overlay
    defined a backdrop color varibale and a mask element

--- react-exercises/modal-react/src/Components/Modal/index.js
+++ react-exercises/modal-react/src/Components/Modal/index.js
@@ -1,19 +1,20 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import './styles.scss'
+import React from "react";
+import PropTypes from "prop-types";
+import "./styles.scss";
 
 const Modal = () => {
-    return (
-        <div className="modal-wrap">
-            <div className="modal">
-                test modal
-            </div>
-        </div>
-    )
-}
+  return (
+    <React.Fragment>
+      <div className="modal-wrap">
+        <div className="modal-mask"></div>
+        <div className="modal">test modal</div>
+      </div>
+    </React.Fragment>
+  );
+};
 
 Modal.propTypes = {
-    prop: PropTypes
-}
+  prop: PropTypes,
+};
 
-export {Modal}
+export { Modal };
