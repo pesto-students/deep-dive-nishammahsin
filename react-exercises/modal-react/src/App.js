0ed8396b8af26bcd6ad0ae0bb31579c68f0b10b3
Nisham <nisham.mahsin@gmail.com>
2020-04-30 13:53:29 +0530

    make modal centered
    always make the modal centered

--- react-exercises/modal-react/src/App.js
+++ react-exercises/modal-react/src/App.js
@@ -5,6 +5,8 @@ import { Modal } from "./Components/Modal";
 function App() {
   return (
     <div className="App">
+     test
+     lorem lorem
       <Modal></Modal>
     </div>
   );
--- react-exercises/modal-react/src/Components/Modal/index.js
+++ react-exercises/modal-react/src/Components/Modal/index.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import './styles.scss'
 
 const Modal = () => {
     return (
-        <div>
-            This is test modal
+        <div className="modal-wrap">
+            <div className="modal">
+                test modal
+            </div>
         </div>
     )
 }
